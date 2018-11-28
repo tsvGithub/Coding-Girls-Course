70f0ca06c7a50b929d3e3135ee7e577c08966a0a
tsvGithub <43065594+tsvGithub@users.noreply.github.com>
2018-11-28 18:35:32 +0200

    button's color change added

--- script.js
+++ script.js
@@ -5,20 +5,22 @@ function checkEvent(event){
 
     var name=document.getElementById("name");  
     var valueName=name.value;
-
     if(valueName === ''){
         name.style.border="1px solid red";
-        console.log('Name is not entered');
+        console.log('Required field: Name is not entered');
+        joinButton.style.background="red";
+        joinButton.style.border="1px solid red";
     } else{
         name.style.border="1px solid #a2ca28";
     }
 
     var email=document.getElementById('email');
     var valueEmail=email.value;
-
     if (valueEmail === '') {
         email.style.border="1px solid red";
-        console.log('Email is not entered.');
+        console.log('Required field: Email is not entered.');
+        joinButton.style.background="red";
+        joinButton.style.border="1px solid red";
     } else{
         email.style.border="1px solid #a2ca28"; 
     }
